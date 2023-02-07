49d2644e31e1874936c3d67112765464a9b99e98
FractalEcho <fractalalia@proton.me>
2023-02-07 10:06:40 -0500

    Update MouseLook.cs
    Fixed mouselook to work with parent object.

--- EyedleGame/Assets/Scripts/MouseLook.cs
+++ EyedleGame/Assets/Scripts/MouseLook.cs
@@ -7,12 +7,15 @@ public class MouseLook : MonoBehaviour
 
     public float mouseSensitivity = 100.0f;
     public float clampAngle = 80.0f;
+    public GameObject root;
 
     private float rotY = 0.0f; // rotation around the up/y axis
     private float rotX = 0.0f; // rotation around the right/x axis
 
     void Start()
     {
+        root = this.transform.parent.gameObject;
+
         Vector3 rot = transform.localRotation.eulerAngles;
         rotY = rot.y;
         rotX = rot.x;
@@ -30,6 +33,12 @@ public class MouseLook : MonoBehaviour
 
         Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
         transform.rotation = localRotation;
+
+        if (root != null)
+        {
+            Quaternion rootRotation = Quaternion.Euler(rotX, 0.0f, 0.0f);
+            root.transform.rotation = rootRotation;
+        }
 
     }
 }
